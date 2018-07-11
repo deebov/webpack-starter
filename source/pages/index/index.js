562d46875742d90b8044fef7702a2dc1ced5ff9d
Dilshod Turobov <deebov@yandex.com>
2018-07-12 00:55:17 +0500

    first commit

---
+++ source/pages/index/index.js
@@ -0,0 +1,6 @@
+import createMenu from '../../components/menu/menu';
+var menu = createMenu(['Главнfая','Обо мне','Портфолио','Блог'],'menu');
+document.body.appendChild(menu);
+
+
+console.log('index');
